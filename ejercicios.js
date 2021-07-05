67d7053c17f3f295f41544ef373b5d679b8d4fe6
santiagosaccc <santiagosacc2006@gmail.com>
2021-07-04 23:40:47 -0500

    new folder

---
+++ ejercicios.js
@@ -0,0 +1,12 @@
+//operaciones aritmeticas
+let numeroUno = 7;
+let numeroDos = 6;
+
+let suma , resta , multiplicacion , divicion ;
+suma = numeroUno + numeroDos;
+resta = numeroUno - numeroDos;
+multiplicacion = numeroUno * numeroDos;
+divicion = numeroUno / numeroDos;
+
+console.log(suma,resta,divicion)
+
